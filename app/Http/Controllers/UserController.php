497e7c5685c850c857872d5d7e2917d0ac540548
bitmarksss <bitmarksss@gmail.com>
2022-10-26 10:23:05 +0800

    test_

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,7 +7,6 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
     public function User()
     {
         return view('admin.user.manage');
@@ -16,11 +15,14 @@ class UserController extends Controller
     public function searchUser(Request $request)
     {
         $search_string = $request->search_string;
-        
-        $users = DB::table('users')
-        ->where('usr_full_name','LIKE', $search_string . '%')
-        ->where('acc_id','=',session('acc_id'))
-        ->orderBy('usr_full_name')
-        ->get();
+        session()->flash('success','test');
+        // $users = DB::table('users')
+        // ->where('usr_full_name','LIKE', $search_string . '%')
+        // ->where('acc_id','=',session('acc_id'))
+        // ->orderBy('usr_full_name')
+        // ->get();
+
+        return view('admin.employees.manage',compact('employees','search_string','modules'));
+
     }
 }
