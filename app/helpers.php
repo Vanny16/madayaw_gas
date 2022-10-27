215c16656ffb21bb882ea3a9f50f0c2000830a4a
bitmarksss <bitmarksss@gmail.com>
2022-10-27 11:54:28 +0800

    added function for generating uuid

--- app/helpers.php
+++ app/helpers.php
@@ -1,5 +1,15 @@
 <?php
 
+function generateuuid()
+    { 
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyz';
+        $string = '';
 
+        for ($i = 0; $i < 32; $i++) {
+            $string .= $characters[mt_rand(0, strlen($characters) - 1)];
+        }
+
+        return $string;
+    }
 
 ?>
