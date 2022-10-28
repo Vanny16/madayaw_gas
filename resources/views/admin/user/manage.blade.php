2a1f157913581693e7f5d278bbdb5298d843b30a
DarkPalX <raevinjhonpalx@gmail.com>
2022-10-28 15:50:57 +0800

    new comms

--- resources/views/admin/user/manage.blade.php
+++ resources/views/admin/user/manage.blade.php
@@ -142,9 +142,6 @@
                                                         <a class="fa fa-toggle-on" type="button" href="{{ action('UserController@deactivateUser',[$user->usr_id]) }}" aria-hidden="true"></a>
                                                     </td>
                                                 @endif
-                                                @if($user->usr_active == 0)
-                                                <td>-</td>
-                                                @else
                                                 <td>
                                                     <div class="dropdown">
                                                         <div class="dropdown">
@@ -156,7 +153,6 @@
                                                         </div>
                                                     </div>
                                                 </td>
-                                                @endif
                                                 <!--Edit User Modal-->
                                                 <div class="modal fade" id="edit-modal-{{$user->usr_id}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
                                                     <div class="modal-dialog modal-lg" role="document">
@@ -263,26 +259,29 @@
                                 </div>
                                 <hr>
                                 <div class="row">
-                                    <div class="col-md-2">
-                                        <label for="typ_id">User Type<span style="color:red;">*</span></label>
-                                        <select class="form-control" name="typ_id" required>
-                                            <option value="1" selected>Admin</option>
-                                            <option value="2">Employee</option>
-                                            <option value="3">Observer</option>
-                                        </select> 
+                                    <div class="col-md-6">
+                                        <i class="fa fa-user-cog fa-10x"></i>
                                     </div>
-                                    <div class="col-md-5">
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label for="typ_id">User Type <span style="color:red;">*</span></label>
+                                            <select class="form-control" name="typ_id" required>
+                                                <option value="1" selected>Admin</option>
+                                                <option value="2">Employee</option>
+                                                <option value="3">Observer</option>
+                                            </select> 
+                                        </div>
+
                                         <div class="form-group">
                                             <label for="usr_name">Username <span style="color:red">*</span></label>
                                             <input type="text" class="form-control" name="usr_name" placeholder="Enter Username" value="{{ old('usr_name') }}" required/>
                                         </div>
-                                    </div>    
-                                    <div class="col-md-5">
+                                        
                                         <div class="form-group">
                                             <label for="usr_password">Password <span style="color:red">*</span></label>
                                             <input type="password" class="form-control" name="usr_password" placeholder="Enter Password" value="{{ old('usr_password') }}" required/>
                                         </div>
-                                    </div>
+                                    </div>    
                                 </div>
                             </div>
                             <div class="modal-footer">
