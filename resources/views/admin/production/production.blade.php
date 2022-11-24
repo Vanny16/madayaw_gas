764f59c2148d36f178b3284db73b49b8b33a519e
purge710 <johnrussel338@gmail.com>
2022-11-24 16:08:19 +0800

    push

--- resources/views/admin/production/production.blade.php
+++ resources/views/admin/production/production.blade.php
@@ -327,7 +327,7 @@
                                                 <div class="modal-dialog modal-md show" role="document">
                                                     <div class="modal-content">
                                                         <div class="modal-header">
-                                                            <h5 class="modal-title" id="exampleModalLabel">Raw Materials</h5>
+                                                            <h5 class="modal-title" id="exampleModalLabel">Empty Canisters</h5>
                                                             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                                                 <span aria-hidden="true">&times;</span>
                                                             </button>
