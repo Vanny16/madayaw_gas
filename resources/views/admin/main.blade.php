8a1f1f5a09e7cef2982c2c0af8506c66d814660c
purge710 <johnrussel338@gmail.com>
2022-11-25 16:20:43 +0800

    push

--- resources/views/admin/main.blade.php
+++ resources/views/admin/main.blade.php
@@ -26,136 +26,177 @@
             </div>
 
 			<div class="row">
-				{{-- <div class="col-md-9"> 
-                    <div class="card">
-                        <div class="card-header">
-                            <h3 class="card-title text-info"><i class="fa fa-bolt"></i> Quick Access</h3>
-                        </div>
-
-						<div class="card-body bg-light">
-							<div class="row justify-content-center p-1 text-white">
-								<!-- <div class="col-3 bg-info border border-light rounded">
-									<button class="btn btn-info" width="100vw" height="100vh"><i class="fa fa-box-open"></i></button><br><small>Products</small>
-								</div>
-								<div class="col-3 bg-info border border-light rounded">
-									<button class="btn btn-info" width="100vw" height="100vh"><i class="fa fa-warehouse"></i></button><br><small>Suppliers</small>
-								</div>
-								<div class="col-3 bg-info border border-light rounded">
-									<button class="btn btn-info" width="100vw" height="100vh"><i class="fa fa-address-card"></i></button><br><small>Users</small>
-								</div>
-								<div class="col-3 bg-info border border-light rounded">
-									<button class="btn btn-info" width="100vw" height="100vh"><i class="fa fa-users"></i></button><br><small>Customers</small>
-								</div> -->
-
-								<a class="btn btn-info rounded-circle mr-1" width="100vw" height="100vh"><i class="fa fa-box-open"></i></a>
-								<a class="btn btn-info rounded-circle mr-1" width="100vw" height="100vh"><i class="fa fa-warehouse"></i></a>
-								<a class="btn btn-info rounded-circle mr-1" width="100vw" height="100vh"><i class="fa fa-address-card"></i></a>
-								<a class="btn btn-info rounded-circle mr-1" width="100vw" height="100vh"><i class="fa fa-users"></i></a><br>
+				<div class="col-md-8">
+					<div class="col-md-12"> 
+						<div class="card">
+							<div class="card-header">
+								<h3 class="card-title text-info"><i class="fa fa-bolt"></i> Quick Access</h3>
 							</div>
-                        </div>
-                    </div>
-                </div> --}}
 
-                <div class="col-md-9"> 
+							<div class="card-body bg-light">
+								<div class="row justify-content-center p-1 text-white">
+										
+										<div class="col-3 bg-info border border-light rounded">
+											<a href="{{ action('SalesController@main') }}" class="btn btn-info" width="100vw" height="100vh"><i class="nav-icon fas fa-cash-register"></i></a></button><br><small>Point of Sale</small>
+										</div>
+										<div class="col-3 bg-info border border-light rounded">
+											<a href="{{ action('ProductController@manage') }}" class="btn btn-info" width="100vw" height="100vh"><i class="fa fa-box-open"></i></a></button><br><small>Products</small>
+										</div>
+										<div class="col-3 bg-info border border-light rounded">
+											<a href="{{ action('SupplierController@manage') }}" class="btn btn-info" width="100vw" height="100vh"><i class="fa fa-warehouse"></i></a></button><br><small>Suppliers</small>
+										</div>
+										<div class="col-3 bg-info border border-light rounded">
+											<a href="{{ action('SalesController@main') }}" class="btn btn-info" width="100vw" height="100vh"><i class="nav-icon fa fa-bar-chart"></i></a></button><br><small>Sales Report</small>
+										</div>
+										<div class="col-3 bg-info border border-light rounded">
+											<a href="{{ action('ProductionController@tank') }}" class="btn btn-info" width="100vw" height="100vh"><i class="nav-icon fa fa-gas-pump"></i></a></button><br><small>Tank</small>
+										</div>
+										<div class="col-3 bg-info border border-light rounded">
+											<a href="{{ action('ProductionController@manage') }}" class="btn btn-info" width="100vw" height="100vh"><i class="nav-icon fas fa-pallet"></i></a></button><br><small>Production</small>
+										</div>
+										<div class="col-3 bg-info border border-light rounded">
+											<a href="{{ action('UserController@user') }}" class="btn btn-info" width="100vw" height="100vh"><i class="fa fa-id-card"></i></a></button><br><small>Users</small>
+										</div>
+										<div class="col-3 bg-info border border-light rounded">
+											<a href="{{ action('CustomerController@manage') }}" class="btn btn-info" width="100vw" height="100vh"><i class="nav-icon fas fa-user-tag"></i></a></button><br><small>Customers</small>
+										</div>
+									
+									<!-- <a class="btn btn-info rounded-circle mr-1" width="100vw" height="100vh"><i class="fa fa-box-open"></i></a>
+									<a class="btn btn-info rounded-circle mr-1" width="100vw" height="100vh"><i class="fa fa-warehouse"></i></a>
+									<a class="btn btn-info rounded-circle mr-1" width="100vw" height="100vh"><i class="fa fa-address-card"></i></a>
+									<a class="btn btn-info rounded-circle mr-1" width="100vw" height="100vh"><i class="fa fa-users"></i></a><br> -->
+								</div>
+							</div>
+						</div>
+					</div>
+
+					<div class="col-md-12"> 
+						<div class="card">
+							<div class="card-header">
+								<h3 class="card-title"><i class="fa fa-bullhorn"></i> News and Announcements</h3>
+								<div class="card-tools">
+									<button type="button" class="btn btn-tool" data-card-widget="maximize"><i class="fas fa-expand"></i></button>
+								</div>
+							</div>
+
+							<div class="card-body">
+								<div class="timeline">
+									<div class="time-label">
+										<span class="text-gray ml-4"><i class="fa fa-thumbtack"></i></span>
+										<button type="button" class="btn btn-primary btn-sm float-right" data-toggle="modal" data-target="#createNews"><i class="fa fa-pen mr-2"></i>Compose</button>
+									</div>
+									<div>
+										<i class="fas fa-newspaper bg-blue"></i>
+										<div class="timeline-item">
+											<span class="time"><i class="fas fa-clock"></i> 6 days ago</span>
+											<h3 class="timeline-header"><strong>Welcome</strong></h3>
+											<div class="timeline-body">
+												<div class="thumbnail">
+													<div class="caption ml-3 mt-3">
+														<p>Hello, everyone!.</p>
+													</div>
+													<div class="col-12 text-center">
+														<a href="{{-- asset('images/news/' . $news_item->nws_image) --}}">
+															<img src="{{ asset('img/landing/logo-1.png') }}" alt="ss" style="width:50%"/>
+														</a>
+													</div>
+												</div>
+											</div>
+											<div class="timeline-footer">
+													<a style="color:#dc3545;" href="javascript:void(0)" data-toggle="modal" data-target="#removeNews"><i class="fa fa-trash"></i></a>
+											
+											</div>
+										</div>
+										<div class="modal fade" id="removeNews">
+											<div class="modal-dialog">
+												<div class="modal-content">
+													<div class="modal-header">
+														<h4 class="modal-title">Confirm Deletion</h4>
+														<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+													</div>
+													<div class="modal-body">
+														<p>Are you sure you want to delete this news/announcement?</p>
+													</div>
+													<div class="modal-footer justify-content-between">
+														<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+														<a class="btn btn-danger btn-sm" href=""><i class="fa fa-trash"></i> Delete</a>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				
+				<div class="col-md-4"> 
                     <div class="card">
                         <div class="card-header">
-                            <h3 class="card-title"><i class="fa fa-bullhorn"></i> News and Announcements</h3>
+                            <h3 class="card-title"><i class="fas fa-clock"></i> Production Summary</h3>
                             <div class="card-tools">
                                 <button type="button" class="btn btn-tool" data-card-widget="maximize"><i class="fas fa-expand"></i></button>
                             </div>
                         </div>
-
-						<div class="card-body">
-                            <div class="timeline">
-                                <div class="time-label">
-                                    <span class="text-gray ml-4"><i class="fa fa-thumbtack"></i></span>
-									<button type="button" class="btn btn-primary btn-sm float-right" data-toggle="modal" data-target="#createNews"><i class="fa fa-pen mr-2"></i>Compose</button>
+                        <!-- Opening -->
+                        <div class="card-body" style="overflow-x:auto;">
+                            <div class="row mb-3">
+                                <div class="col-7">
+                                    <small class="text-success">Opening Operations</small>
                                 </div>
-								<div>
-									<i class="fas fa-newspaper bg-blue"></i>
-									<div class="timeline-item">
-										<span class="time"><i class="fas fa-clock"></i> 6 days ago</span>
-										<h3 class="timeline-header"><strong>Welcome</strong></h3>
-										<div class="timeline-body">
-											<div class="thumbnail">
-												<div class="caption ml-3 mt-3">
-													<p>Hello, everyone!.</p>
-												</div>
-												<div class="col-12 text-center">
-													<a href="{{-- asset('images/news/' . $news_item->nws_image) --}}">
-														<img src="{{ asset('img/landing/logo-1.png') }}" alt="ss" style="width:50%"/>
-													</a>
-												</div>
-											</div>
-										</div>
-										<div class="timeline-footer">
-												<a style="color:#dc3545;" href="javascript:void(0)" data-toggle="modal" data-target="#removeNews"><i class="fa fa-trash"></i></a>
-										
-										</div>
-									</div>
-									<div class="modal fade" id="removeNews">
-										<div class="modal-dialog">
-											<div class="modal-content">
-												<div class="modal-header">
-													<h4 class="modal-title">Confirm Deletion</h4>
-													<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-												</div>
-												<div class="modal-body">
-													<p>Are you sure you want to delete this news/announcement?</p>
-												</div>
-												<div class="modal-footer justify-content-between">
-													<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-													<a class="btn btn-danger btn-sm" href=""><i class="fa fa-trash"></i> Delete</a>
-												</div>
-											</div>
-										</div>
-									</div>
-								</div>
                             </div>
+                            <table class="table table-hover table-condensed">
+                                <thead>
+                                    <tr>
+                                        <th width="50px">#</th>
+                                        <th>Tank Name</th>
+                                        <th>Time Start</th>
+                                        <th>Tank Opening</th>
+                                    </tr>
+                                </thead>
+                                <tbody id="tbl-products">
+                                    <tr>
+                                        <td class="text-danger">1</td>
+                                        <td>Tank 1</td>
+                                        <td>6:00 AM</td>
+                                        <td>5000 kgs</td>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+
+                        <hr>
+                        <!-- Closing -->
+                        <div class="card-body" style="overflow-x:auto;">
+                            <div class="row mb-3">
+                                <div class="col-8">
+                                    <small class="text-danger">Closing Operations</small>
+                                </div>
+                            </div>
+                            <table class="table table-hover table-condensed">
+                                <thead>
+                                    <tr>
+                                        <th width="50px">#</th>
+                                        <th>Tank Name</th>
+                                        <th>Time End</th>
+                                        <th>Tank Closing</th>
+                                    </tr>
+                                </thead>
+                                <tbody id="tbl-products">
+                                    <tr>
+                                        <td class="text-danger">1</td>
+                                        <td>Tank 1</td>
+                                        <td>3:00 PM</td>
+                                        <td>2567 kgs</td>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
                         </div>
                     </div>
                 </div>
-				
-				<div class="col-md-3">
-					<div class="card card-info">
-                        <div class="card-header border-bottom-0 bg-navy">
-                            <h3 class="card-title"><i class="fas fa-users"></i> Recent users</h3>
-                        </div>
-                        <div class="card-body pt-0">
-                            <ul class="products-list product-list-in-card pl-2 pr-2">
-                                    <li class="item">
-                                        <div class="product-img"> 
-                                            <img class="img-size-50 img-circle" src="" alt="logs user image">
-                                        </div>
-                                        <div class="product-info">
-                                            <a href="#" class="product-title">
-                                                <span class="badge badge-info float-right"></span>
-                                            </a>
-                                            <span class="product-description">
-                                            </span> 
-                                        </div>
-                                    </li>
-                                    <li class="item">
-                                        <div class="product-img"> 
-                                            
-                                        </div>
-                                        <div class="product-info">
-                                            <a href="#" class="product-title">
-                                                <span class="badge badge-info float-right"></span>
-                                            </a>
-                                            <span class="product-description">
-                                                No logs for today...
-                                            </span> 
-                                        </div>
-                                    </li>
-                            </ul>
-                        </div> 
-                        <div class="card-footer text-center">
-                            
-                        </div>
-                    </div>
-				</div>
-
             </div>
 	</section>
 
@@ -202,7 +243,7 @@
 			</div>
 		</div>
 	</div>
-
+</div>
 <script>
     $(".custom-file-input").on("change", function() {
         var fileName = $(this).val().split("\\").pop();
