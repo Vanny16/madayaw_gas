0d1689f49435a88346728cfaec775a468ae04945
purge710 <johnrussel338@gmail.com>
2023-01-17 15:28:34 +0800

    push

--- resources/views/admin/customers/manage.blade.php
+++ resources/views/admin/customers/manage.blade.php
@@ -390,14 +390,16 @@
 
                             <label> Select Accessible Products <span style="color:red">*</span></label>
                             <div class="card">
-                                <div class="col-12 p-1">
-                                    <div class="col-md-6">
-                                        <input type="checkbox" id="" name="" value="">
-                                        <label for=""> Madayaw Round</label>
-                                    </div>
-                                    <div class="col-md-6">
-                                        <input type="checkbox" id="" name="" value="">
-                                        <label for=""> Madayaw Sqaure</label>
+                                <div class="col-12 p-2">
+                                    <div class="row">
+                                        <div class="col-md-6">
+                                            <input type="checkbox" id="" name="" value="">
+                                            <label for=""> Madayaw Round</label>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <input type="checkbox" id="" name="" value="">
+                                            <label for=""> Madayaw Sqaure</label>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
