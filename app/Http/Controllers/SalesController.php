4dcee46274c8ea00b8bb13da94e8ea9c2f68c009
bitmarksss <bitmarksss@gmail.com>
2023-01-19 13:11:34 +0800

    function for adding opposition canister

--- app/Http/Controllers/SalesController.php
+++ app/Http/Controllers/SalesController.php
@@ -140,4 +140,81 @@ class SalesController extends Controller
         session()->flash('successMessage','Transaction complete!');
         return redirect()->action('SalesController@main');
     }
+
+    public function addCanister(Request $request)
+    {
+        $cus_name = $request->cus_name;
+        $cus_address = $request->cus_address;
+        $cus_contact = $request->cus_contact;
+        $cus_notes = $request->cus_notes;
+        $cus_accessible = $request->cus_accessible;
+
+        // dd($cus_accessible);
+
+        $check_cus_name = DB::table('customers')
+        ->where('acc_id', '=', session('acc_id'))
+        ->where('cus_name','=', $cus_name)
+        ->first();
+
+        if($check_cus_name != null)
+        {
+            session()->flash('errorMessage','Customer name is already existing');
+            return redirect()->action('CustomerController@manage');
+        }
+
+        $usr_id = DB::table('customers')
+        ->insert([
+        'acc_id' => session('acc_id'),
+        'cus_uuid' => generateuuid(),
+        'cus_name' => $cus_name, 
+        'cus_address' => $cus_address,
+        'cus_contact' => $cus_contact,
+        'cus_notes' => $cus_notes
+        ]);
+
+        //IMAGE UPLOAD 
+        if($request->file('cus_image'))
+        {
+            $cus_id = DB::table('customers')
+            ->select('cus_id')
+            ->orderBy('cus_id', 'desc')
+            ->first();
+    
+            $file = $request->file('cus_image');
+
+            $validator = Validator::make( 
+                [
+                    'file' => $file,
+                    'extension' => strtolower($file->getClientOriginalExtension()),
+                ],
+                [
+                    'file' => 'required',
+                    'file' => 'max:3072', //3MB
+                    'extension' => 'required|in:jpg,png,gif',
+                ]
+            );
+            
+            // dd(public_path());
+    
+            if ($validator->fails()) 
+            {
+                session()->flash('errorMessage',  "Invalid File Extension or maximum size limit of 5MB reached!");
+                return redirect()->back()->withErrors($validator)->withInput();
+            }
+    
+            $fileName = $cus_id->cus_id . '.' . $file->getClientOriginalExtension();
+    
+            Storage::disk('local')->put('img/customers/' . $fileName, fopen($file, 'r+'));
+
+            DB::table('customers')
+            ->where('cus_id','=',$cus_id->cus_id)
+            ->update([
+                'cus_image' => $fileName,
+            ]);  
+    
+        }   
+
+        session()->flash('successMessage','New customer has been added');
+        return redirect()->action('CustomerController@manage');
+    }
 }
