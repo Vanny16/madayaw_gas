7983a946b7e80c31adf494ee999a0a1aa4f9c855
purge710 <johnrussel338@gmail.com>
2023-01-30 14:28:31 +0800

    push

--- resources/views/admin/sales/main.blade.php
+++ resources/views/admin/sales/main.blade.php
@@ -225,10 +225,10 @@
                                                                     <input type="number" class="form-control" id="loose_amount{{$product->prd_id}}" value="0" min="" max="{{$product->prd_quantity}}" onkeyup="getTotal(prd_price{{$product->prd_id}}.id, crates_amount{{$product->prd_id}}.id, loose_amount{{$product->prd_id}}.id, temp_discount{{$product->prd_id}}.id, sub_total{{$product->prd_id}}.id)" onkeypress="return isNumberKey(this, event);" onclick="this.select()" onchange="noNegativeValue('loose_amount{{$product->prd_id}}')" required></input>
                                                             </div>
                                                             @else
-                                                                <div class="form-group col-6">
+                                                                <!-- <div class="form-group col-6">
                                                                     <label for="cus_address"># of Crates <span style="color:red">*</span></label>
                                                                     <input type="hidden" class="form-control" id="crates_amount{{$product->prd_id}}" value="0" min="" max="{{$product->prd_quantity}}" onkeyup="getTotal(prd_price{{$product->prd_id}}.id, crates_amount{{$product->prd_id}}.id, loose_amount{{$product->prd_id}}.id, temp_discount{{$product->prd_id}}.id, sub_total{{$product->prd_id}}.id)" onkeypress="return isNumberKey(this, event);" onclick="this.select()"  onchange="noNegativeValue('crates_amount{{$product->prd_id}}')" required></input>
-                                                                </div>
+                                                                </div> -->
                                                                 <div class="form-group col-12">
                                                                     <label for="cus_address">Quantity <span style="color:red">*</span></label>
                                                                     <input type="number" class="form-control" id="quantity{{$product->prd_id}}" value="0" min="" max="{{$product->prd_quantity}}" onkeyup="getTotal(prd_price{{$product->prd_id}}.id, crates_amount{{$product->prd_id}}.id, loose_amount{{$product->prd_id}}.id, temp_discount{{$product->prd_id}}.id, sub_total{{$product->prd_id}}.id)" onkeypress="return isNumberKey(this, event);" onclick="this.select()" onchange="noNegativeValue('loose_amount{{$product->prd_id}}')" required></input>
