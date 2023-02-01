fab70175121d162e4203f38f234e61cb446d3a22
purge710 <johnrussel338@gmail.com>
2023-02-01 11:28:44 +0800

    push

--- resources/views/admin/print/deliveryreceipt.blade.php
+++ resources/views/admin/print/deliveryreceipt.blade.php
@@ -8,12 +8,12 @@
     <div class="col-md-12"> 
         <div class="card">
             <div class="card-header">
-                <p style="text-align:center;"> MADAYAW PETROLEUM AND GAS CORPORATION</p>
+                <p style="text-align:center;"><strong> MADAYAW PETROLEUM AND GAS CORPORATION</strong></p>
                 <p style="text-align:center;"> PARK Avenue Cor. Lakatan St., Brgy. Wilfredo Aquino, Agdao, Davao City<p>
             </div>
             <div class="row">
                 <div class="col-md-2 col-12 mb-3">
-                    <p>DELIVERY RECEIPT </p>
+                    <p><strong>DELIVERY RECEIPT </strong></p>
                 </div>
                 <div class="col-md-8 col-12 mb-6">
                     <p class="text-danger fa-2x mr-2" style="text-align:right;">NO. </p>
@@ -22,28 +22,28 @@
        
         </div>
         
-            <p>Customer Name: </p>
-            <p>Date: </p>
-            <p>Address: </p>
+            <p><strong>Customer Name: </strong></p>
+            <p><strong>Date: </strong></p>
+            <p><strong>Address: </strong></p>
 
             <div class="card-body">
                 <table class="table table-hover table-condensed">
                     <thead>
                         <tr>
-                            <th width="500px">QTY</th>
-                            <th width="500px">Unit</th>
-                            <th width="500px">Description</th>
-                            <th width="500px">Unit Price</th>
-                            <th>Amount</th>
+                            <th width="500px" style="border: 1px solid black; border-collapse:collapse;">QTY</th>
+                            <th width="500px" style="border: 1px solid black; border-collapse:collapse;">Unit</th>
+                            <th width="500px" style="border: 1px solid black; border-collapse:collapse;">Description</th>
+                            <th width="500px" style="border: 1px solid black; border-collapse:collapse;">Unit Price</th>
+                            <th style="border: 1px solid black; border-collapse:collapse;">Amount</th>
                             <th width="20px"></th>
                         </tr>
                     </thead>
                     <tbody>
                         <tr class="bg-light" height="1px">
-                            <td colspan="8"></td>
+                            <td colspan="5" style="border: 1px solid black; border-collapse:collapse;"></td>
                         </tr>
                         <tr class="text-success bg-white">
-                            <td colspan="5"></td>
+                            <td colspan="3"></td>
                             <td class="text-success"><strong>Total</strong></td>
                             <td class="text-success"><strong id="lbl_total" class="fa fa-2x">0.00</strong></td>
                         </tr>
