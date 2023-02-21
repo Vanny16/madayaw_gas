c4ce92e6bfaa45d409d4357a657780d0860a2f4c
purge710 <johnrussel338@gmail.com>
2023-02-21 14:22:49 +0800

    push for sales reports update

--- resources/views/admin/reports/sales.blade.php
+++ resources/views/admin/reports/sales.blade.php
@@ -106,50 +106,81 @@
                                 <table class="table table-hover table-condensed">
                                     <thead>
                                         <tr>
-                                            <th>Product Name</th>
-                                            <th>Description</th>
-                                            <th>Price</th>
-                                            <th>Quantity Sold</th>
-                                            <th>Total Sales</th>
-                                            <th></th>
+                                            <th>Reference ID</th>
+                                            <th>User</th>
+                                            <th>Customer</th>
+                                            <th>Date & Time</th>
+                                            <th>Total Sale</th>
+                                            <th>Amount Received</th>
+                                            <th>Change</th>
                                         </tr>
                                     </thead>
-                                    <tbody id="tbl-cart">
+                                    <tbody>
                                         @php($total_sold = 0)
                                         @php($total_sales = 0)
                                         @foreach($sales as $sale)
-                                        
-                                           @if($sale->total_sold != null)
-                                                @php($total_sold += $sale->total_sold)
-                                            @endif
-                                            @if($sale->total_sales != null)
-                                                @php($total_sales += $sale->total_sales)
-                                            @endif
-
-                                            @if($sale->total_sold == null)
-                                                @php($sale->total_sold = "0")
-                                            @endif
-                                            @if($sale->total_sales == null)
-                                                @php($sale->total_sales = "0")
-                                            @endif
-                                            
-                                            <tr>
-                                                <td>{{ $sale->prd_name }}</td>
-                                                <td>{{ $sale->prd_description }}</td>
-                                                <td>₱ {{ number_format($sale->prd_price, 2, '.', ',') }}</td>
-                                                <td>{{ number_format($sale->total_sold, 2, '.', ',') }}</td>
-                                                <td>₱ {{ number_format($sale->total_sales, 2, '.', ',') }}</td>
-                                                <td></td>
+                                            <tr class='clickable-row' data-toggle="modal" data-target="#purchases-modal{{ $sale->trx_ref_id }}" >
+                                                <td>{{ $sale->trx_ref_id }}</td>
+                                                <td>{{ $sale->usr_full_name }}</td>
+                                                <td>{{ $sale->cus_name }}</td>
+                                                <td>{{ $sale->trx_datetime }}</td>
+                                                <td>₱ {{ number_format($sale->trx_total, 2, '.', ',') }}</td>
+                                                <td>₱ {{ number_format($sale->trx_amount_paid, 2, '.', ',') }}</td>
+                                                <td>₱ {{ number_format($sale->trx_balance, 2, '.', ',') }}</td>
                                             </tr>
+
+                                            <!-- Purchases Modal -->
+                                            <div class="modal fade" id="purchases-modal{{ $sale->trx_ref_id }}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                                <div class="modal-dialog modal-lg" role="document">
+                                                    <div class="modal-content">
+                                                        <div class="modal-header text-info">
+                                                            <h5 class="modal-title"><i class="fa fa-receipt mr-2"> </i>Purchases</h5>
+                                                            <p class="text-danger mr-2">TRX {{ $sale->trx_ref_id }}</p>
+                                                        </div> 
+                                                        <div class="modal-body">
+                                                            <div class="col-12">
+                                                                <div class="row">
+                                                                    <div class="container">
+                                                                        <div class="row header">
+                                                                            <div class="col"><strong>Product</strong></div>
+                                                                            <div class="col"><strong>Price</strong></div>
+                                                                            <div class="col"><strong>Crates</strong></div>
+                                                                            <div class="col"><strong>Loose</strong></div>
+                                                                            <div class="col"><strong>Deposit</strong></div>
+                                                                            <div class="col"><strong>Subtotal</strong></div>
+                                                                        </div>
+                                                                        @foreach($purchases as $purchase)
+                                                                            @if($purchase->trx_id == $sale->trx_id)
+                                                                                <hr>
+                                                                                <div class="row">
+                                                                                    <div class="col">{{ $purchase->prd_name }}</div>
+                                                                                    <div class="col">₱ {{ number_format($purchase->prd_price, 2, '.', ',') }}</div>
+                                                                                    <div class="col">{{ number_format($purchase->pur_crate, 0, '', ',') }}</div>
+                                                                                    <div class="col">{{ number_format($purchase->pur_loose, 0, '', ',') }}</div>
+                                                                                    <div class="col">₱ {{ number_format($purchase->pur_deposit, 2, '.', ',') }}</div>
+                                                                                    <div class="col">₱ {{ number_format($purchase->pur_total, 2, '.', ',') }}</div>
+                                                                                </div>
+                                                                            @endif
+                                                                        @endforeach
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                        <div class="modal-footer">
+                                                            <form method="POST" action="{{ action('PrintController@allpurchasesReports')}}">
+                                                            {{ csrf_field() }}
+                                                                <button type="submit" class="btn btn-info float-left" href="" target="_BLANK"><i class="fa fa-print"></i> Print</button>
+                                                                <input type="date_from" class="form-control" id="sales_date_from" name="sales_date_from" value="{{ $date_from }}" hidden/>
+                                                                <input type="date_to" class="form-control" id="sales_date_to" name="sales_date_to" value="{{ $date_to }}" hidden/>
+                                                            </form>
+                                                            <button type="submit" class="btn btn-default" data-dismiss="modal"><i class="fa fa-times mr-1"> </i> Close</button>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
                                         @endforeach
                                         <tr class="bg-light" height="1px">
-                                            <td colspan="6"></td>
-                                        </tr>
-                                        <tr>
-                                            <td colspan="3" class=""><strong class="fa-2x">Total</strong></td>
-                                            <td><strong class="fa-2x text-success">{{ number_format($total_sold, 2, '.', ',') }}</strong></td>
-                                            <td><strong class="fa-2x text-success">₱ {{ number_format($total_sales, 2, '.', ',') }}</strong></td>
-                                            <td></td>
+                                            <td colspan="7"></td>
                                         </tr>
                                     </tbody>
                                 </table>
