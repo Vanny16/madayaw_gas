ed7bd09bbc97e7ff3e7ef60cbf18956e5317f4fa
bitmarksss <bitmarksss@gmail.com>
2023-03-29 16:38:40 +0800

    sales controller fix

--- app/Http/Controllers/SalesController.php
+++ app/Http/Controllers/SalesController.php
@@ -400,19 +400,22 @@ class SalesController extends Controller
                     'prd_empty_goods' => $add_empty_good_qty
                 ]);
 
-                //ADD QUANTITY TO STOCKS LOGS FOR CANISTER MOVEMENT TRACKING
-                //get quantity of the product in the stocks_logs table
-                $stocks_logs = DB::table('stocks_logs')
-                ->where('prd_id', '=', $prd_id_in)
-                ->first();
-                $stocks_logs_quantity = $stocks_logs->stk_empty_goods;
+                if($products->prd_is_refillable == 1)
+                {
+                    //ADD QUANTITY TO STOCKS LOGS FOR CANISTER MOVEMENT TRACKING
+                    //get quantity of the product in the stocks_logs table
+                    $stocks_logs = DB::table('stocks_logs')
+                    ->where('prd_id', '=', $prd_id_in)
+                    ->first();
+                    $stocks_logs_quantity = $stocks_logs->stk_empty_goods;
 
-                //add stocks_logs quantity to add_empty_good_qty
-                DB::table('stocks_logs')
-                ->where('prd_id', '=', $prd_id_in)
-                ->update([
-                    'stk_empty_goods' => $stocks_logs_quantity + $add_empty_good_qty
-                ]);
+                    //add stocks_logs quantity to add_empty_good_qty
+                    DB::table('stocks_logs')
+                    ->where('prd_id', '=', $prd_id_in)
+                    ->update([
+                        'stk_empty_goods' => $stocks_logs_quantity + $add_empty_good_qty
+                    ]);
+                }
             }
             else{
                 $deduct_qty = (int)$products->prd_quantity - (int)$pur_qty;
