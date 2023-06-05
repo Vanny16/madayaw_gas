a367f7db82c426e4ea933199ccf67d44847e86d4
purge710 <johnrussel338@gmail.com>
2023-06-05 13:11:56 +0800

    push

--- resources/views/admin/print/badorderreceipt.blade.php
+++ resources/views/admin/print/badorderreceipt.blade.php
@@ -72,15 +72,13 @@
                 <td colspan="1"><strong>{{ $bo_qty }}</strong></td>
             </tr>
             <tr>
-                <td colspan="5"><br></td>
+                <td colspan="5"><br><br><br></td>
             </tr>
             <tr>
-                <td colspan="2">Issued by:</td>
-                <td colspan="3">{{session('usr_full_name')}}</td>
+                <td colspan="5"> <span class="">_______________________<span></td>
             </tr>
             <tr>
-                <td colspan="2">Received by:</td>
-                <td colspan="3 ">{{ $bad_order->cus_name }}</td>
+                <td colspan="5">Signature over printed name</td>
             </tr>
         </table>
     </div>
