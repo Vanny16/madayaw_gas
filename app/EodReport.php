e0c1c3f6f1a4a488bfe7b82f9de45c23be194ff9
bitmarksss <mcalledo225@gmail.com>
2023-12-12 14:35:44 -0600

    improved query

--- app/EodReport.php
+++ app/EodReport.php
@@ -10,16 +10,22 @@ class EodReport extends Model
     protected $guarded = [];
     protected $table = 'eod_reports';
 
-    public static function purchasesReports(){
+    public static function retrieve($reference_ids_array, $production_id = null){
+        $values = [];
+
+        foreach($reference_ids_array as $reference_id){
+            $query = self::whereIn('ref_id', $reference_id);
+
+            if($production_id != null)
+            {
+                $query = $query->where('pdn_id' ,'=', $production_id);
+            }
+
+            array_push($values, $query->get());
+        }
         
+        return $values;
     }
-    public static function issuedReports(){
-        
-    }
-    public static function receivedReports(){
-        
-    }
-    public static function oppositionReports(){
-        
-    }
+    
+
 }
